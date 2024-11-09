13d1b76787f5e46d96fe3ddf4850fc31e090e8e4
iChuck-W <164290338+iChuck-W@users.noreply.github.com>
2024-11-09 13:44:11 +0800

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 
-import { defineConfig } from 'eslint-define-config';
+const { defineConfig } = require('eslint-define-config');
 
-export default defineConfig({
+module.exports = defineConfig({
   env: {
     browser: true,
     es2021: true,
