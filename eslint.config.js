a92dfd86cdd1e31bc902b1497dd8b6d41146c159
iChuck-W <164290338+iChuck-W@users.noreply.github.com>
2024-11-09 13:40:19 +0800

    Create eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+
+import { defineConfig } from 'eslint-define-config';
+
+export default defineConfig({
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    // 自定义规则
+    'react/prop-types': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+});
